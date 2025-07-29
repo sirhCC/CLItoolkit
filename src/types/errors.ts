4ea9b6c47cf6331c4eba5bc1efb6f975e87bb2fa
sirhCC <ccharles199220@gmail.com>
2025-07-29 15:06:27 -0400

    Fix cause property to override in CliError constructor

--- src/types/errors.ts
+++ src/types/errors.ts
@@ -5,7 +5,7 @@ export abstract class CliError extends Error {
   abstract readonly code: string;
   abstract readonly statusCode: number;
   
-  constructor(message: string, public readonly cause?: Error) {
+  constructor(message: string, public override readonly cause?: Error) {
     super(message);
     this.name = this.constructor.name;
     
